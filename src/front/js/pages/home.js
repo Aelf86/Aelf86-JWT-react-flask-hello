a6657b92a01b8f483c145f2145ee056346e1e6b2
Alfredo Hernández <139387685+Aelf86@users.noreply.github.com>
2024-07-13 19:47:49 +0000

    ultimos cambios

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -31,14 +31,14 @@ export const Home = () => {
                     <div className="col">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={name} onChange={(e) => setName(e.target.value)} />
+                            <input type="text" class="form-control" placeholder="Enter your name" id="exampleInputEmail1" aria-describedby="emailHelp" value={name} onChange={(e) => setName(e.target.value)} />
 
                         </div>
                     </div>
                     <div className="col">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Last Name</label>
-                            <input type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+                            <input type="text" class="form-control" placeholder="Enter your last name" id="exampleInputEmail1" aria-describedby="emailHelp" value={lastname} onChange={(e) => setLastname(e.target.value)} />
 
                         </div>
                     </div>
@@ -47,14 +47,14 @@ export const Home = () => {
                     <div className="col">
                         <div class="mb-3">
                             <label for="exampleInputEmail1" class="form-label">Email address</label>
-                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
+                            <input type="email" class="form-control" placeholder="Enter your email" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)} />
                             <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                         </div>
                     </div>
                     <div className="col">
                         <div class="mb-3">
                             <label for="exampleInputPassword1" class="form-label">Password</label>
-                            <input type="password" class="form-control" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)} />
+                            <input type="password" class="form-control" placeholder="Enter your password" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)} />
                         </div>
 
                     </div>
--- src/front/js/pages/login.js
+++ src/front/js/pages/login.js
@@ -28,12 +28,12 @@ export const Login = () => {
             <form>
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Email address</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)}/>
+                    <input type="email" class="form-control" placeholder="Enter your email" id="exampleInputEmail1" aria-describedby="emailHelp" value={email} onChange={(e) => setEmail(e.target.value)}/>
                         <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Password</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)}/>
+                    <input type="password" class="form-control" placeholder="Enter your password" id="exampleInputPassword1" value={password} onChange={(e) => setPassword(e.target.value)}/>
                 </div>
                 
                 
